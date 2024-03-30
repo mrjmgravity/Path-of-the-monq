be7faf95c3cfa22823fbae724ee8eada0b1f4ef7
mrjmgravity <118164654+mrjmgravity@users.noreply.github.com>
2024-03-30 20:42:04 +0100

    funkcia vyherne cisla
    Pridal som funkcou ktora vyhodnoti 6 vyhernych cisiel

--- sportka.py
+++ sportka.py
@@ -1,7 +1,8 @@
-
+import random
 
 def main():
     user_input()
+    winning_number()
 
 
 def user_input(): #tato funkcia vypyta od pouzivatela 6 cisiel a rovno ich aj skontroluje
@@ -23,6 +24,12 @@ def user_input(): #tato funkcia vypyta od pouzivatela 6 cisiel a rovno ich aj sk
             print("Please make sure u entered number from 1 to 35\n")
 
     return numbers
+
+
+def winning_number():
+    win_numbers = random.sample(range(1, 36), 6)
+    print(f"Toto su vyherne cisla:{win_numbers} ")
+    return win_numbers
 
 
 main()
