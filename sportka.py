151c41a70a4dc61981c9185280ed4a1255244a32
mrjmgravity <118164654+mrjmgravity@users.noreply.github.com>
2024-03-30 21:09:09 +0100

    Vyhodnotenie cisiel
    Pridal som vyhodnotenie zadanych cisiel pouzivatelom a vygenerovanych

--- sportka.py
+++ sportka.py
@@ -1,8 +1,10 @@
 import random
 
+
 def main():
-    user_input()
-    winning_number()
+    user_numbers = user_input()
+    win_numbers = winning_number()
+    evaluate(user_numbers,win_numbers)
 
 
 def user_input(): #tato funkcia vypyta od pouzivatela 6 cisiel a rovno ich aj skontroluje
@@ -28,8 +30,15 @@ def user_input(): #tato funkcia vypyta od pouzivatela 6 cisiel a rovno ich aj sk
 
 def winning_number():
     win_numbers = random.sample(range(1, 36), 6)
-    print(f"Toto su vyherne cisla:{win_numbers} ")
+    print(f"Toto su vyherne cisla:{win_numbers} \n")
     return win_numbers
+
+
+def evaluate(random_numbers, user_numbers):
+    win = set(random_numbers) & set(user_numbers)
+    print("Vaše uhádnuté čísla:", win)
+    print("Počet správnych čísel:", len(win))
+
 
 
 main()
